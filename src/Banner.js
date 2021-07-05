4326f1f2d86daf7fe9f163d1d79d2c7695443097
imnayakshubham <imnayakshubham@gmail.com>
2021-07-05 10:36:49 +0530

    video playback added

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./request";
 import "./Banner.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Banner() {
   const [bg, setbg] = useState([]);
+  const [trailerUrl, settrailerUrl] = useState("");
+  const scrollDiv = createRef();
+  console.log(scrollDiv);
+
   useEffect(() => {
     async function fetchData() {
       const req = await axios.get(requests.fetchTrending);
@@ -17,12 +23,39 @@ function Banner() {
     }
     fetchData();
   }, []);
-    const limit = 150;
-    function truncate(str, n) {
-      return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  const limit = 150;
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  const objs = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handletrailer = async (bg) => {
+    // console.log(movie);
+    if (trailerUrl) {
+      settrailerUrl("");
+    } else {
+      movieTrailer(bg?.title || bg?.name || bg.original_name || "")
+        .then((url) => {
+          const urlParam = new URLSearchParams(new URL(url).search);
+          settrailerUrl(urlParam.get("v"));
+        })
+        .catch((err) => console.log(err));
     }
+    // scrollDiv.current.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo(0, scrollDiv.current.offsetTop);
+  };
+
   return (
-    <header>
+    <>
+      <div ref={scrollDiv} className="displayingvideo">
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={objs} />}
+      </div>
       <div
         className="banner"
         style={{
@@ -36,16 +69,17 @@ function Banner() {
             {bg?.title || bg?.original_title || bg?.name}
           </h1>
           <div className="banner_btns">
-            <button className="banner_btn">Play</button>
+            <button className="banner_btn" onClick={() => handletrailer(bg)}>
+              Play
+            </button>
             <button className="banner_btn">My List</button>
           </div>
           <div>
-            <h1 className="banner_desc">{truncate(bg?.overview,225)}
-            </h1>
+            <h1 className="banner_desc">{truncate(bg?.overview, 225)}</h1>
           </div>
         </div>
       </div>
-    </header>
+    </>
   );
 }
 
--- src/Row.js
+++ src/Row.js
@@ -1,36 +1,69 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import "./Row.css";
-
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchUrls, isLargeRow }) {
-  const [display, setdisplay] = useState([]);
+  const scrollDiv = createRef();
+
+  const [movies, setmovies] = useState([]);
+  const [trailerUrl, settrailerUrl] = useState("");
+
   const base_path = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     async function displayingdata() {
       const req = await axios.get(fetchUrls);
-      setdisplay(req.data.results);
+      setmovies(req.data.results);
       return req;
     }
     displayingdata();
   }, [fetchUrls]);
 
+  const objs = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handletrailer = (movie) => {
+    // console.log(movie?.title);
+    if (trailerUrl) {
+      settrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie.original_name || "")
+        .then((url) => {
+          const urlParam = new URLSearchParams(new URL(url).search);
+          settrailerUrl(urlParam.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  console.log(trailerUrl);
   return (
-    <div className="row">
-      <h1>{title}</h1>
-      <div className="row_posters">
-        {display.map((poster, key) => (
-          <img
-            key={poster.id}
-            className={`poster_img ${isLargeRow && "poster_img_large"}`}
-            src={`${base_path}${
-              isLargeRow ? poster.backdrop_path : poster.poster_path
-            }`}
-            alt={poster.name}
-          />
-        ))}
+    <>
+      <div className="row">
+        <h1>{title}</h1>
+        <div className="row_posters">
+          {movies.map((movie, key) => (
+            <img
+              key={movie.id}
+              className={`poster_img ${isLargeRow && "poster_img_large"}`}
+              src={`${base_path}${
+                isLargeRow ? movie.backdrop_path : movie.poster_path
+              }`}
+              alt={movie.name}
+              onClick={() => handletrailer(movie)}
+            />
+          ))}
+        </div>
+        <div ref={scrollDiv} className="displayingvideo">
+          {trailerUrl && <YouTube videoId={trailerUrl} opts={objs} />}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
